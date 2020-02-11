fe73b94ade85cf087e0f8635a6a4edcecb47581a
deva <satriosuklun@gmail.com>
2020-02-11 17:24:35 +0700

    update view

--- app/Http/Controllers/Landing/LandingController.php
+++ app/Http/Controllers/Landing/LandingController.php
@@ -6,24 +6,76 @@ use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 use App\Http\Controllers\Controller;
+use Auth;
 
 
 class LandingController extends Controller
 {
 
 	public function index(){
-		$list = DB::table('buku')
-		->select(DB::raw('buku.*,kategori_buku.nama as namakategori'))
-		->leftjoin('kategori_buku','kategori_buku.id','=','buku.id_kategori')
-		->where('buku.tipe','Book')
-		->orderby('buku.id','desc')
-		->paginate(9);
-		$list_ebook = DB::table('buku')
-		->select(DB::raw('buku.*,kategori_buku.nama as namakategori'))
-		->leftjoin('kategori_buku','kategori_buku.id','=','buku.id_kategori')
-		->where('buku.tipe','Ebook')
-		->orderby('buku.id','desc')
-		->paginate(9);
+		if(Auth::check()){
+			$list = DB::table('buku')
+					->select(DB::raw('buku.*,kategori_buku.nama as namakategori'))
+					->leftjoin('kategori_buku','kategori_buku.id','=','buku.id_kategori')
+					->where('buku.tipe','Book')
+					->orderby('buku.id','desc')
+					->paginate(9);
+	
+					$list_ebook = DB::table('buku')
+					->select(DB::raw('buku.*,kategori_buku.nama as namakategori'))
+					->leftjoin('kategori_buku','kategori_buku.id','=','buku.id_kategori')
+					->where('buku.tipe','Ebook')
+					->orderby('buku.id','desc')
+					->paginate(9);
+		}else{
+			if(Auth::guard('anggota')->check()){
+				if(Auth::guard('anggota')->user()->status_anggota=='Umum'){
+					$list = DB::table('buku')
+					->select(DB::raw('buku.*,kategori_buku.nama as namakategori'))
+					->leftjoin('kategori_buku','kategori_buku.id','=','buku.id_kategori')
+					->where([['buku.tipe','Book'],['buku.umum','ya']])
+					->orderby('buku.id','desc')
+					->paginate(9);
+	
+					$list_ebook = DB::table('buku')
+					->select(DB::raw('buku.*,kategori_buku.nama as namakategori'))
+					->leftjoin('kategori_buku','kategori_buku.id','=','buku.id_kategori')
+					->where([['buku.tipe','Ebook'],['buku.umum','ya']])
+					->orderby('buku.id','desc')
+					->paginate(9);
+				}else{
+					$list = DB::table('buku')
+					->select(DB::raw('buku.*,kategori_buku.nama as namakategori'))
+					->leftjoin('kategori_buku','kategori_buku.id','=','buku.id_kategori')
+					->where('buku.tipe','Book')
+					->orderby('buku.id','desc')
+					->paginate(9);
+	
+					$list_ebook = DB::table('buku')
+					->select(DB::raw('buku.*,kategori_buku.nama as namakategori'))
+					->leftjoin('kategori_buku','kategori_buku.id','=','buku.id_kategori')
+					->where('buku.tipe','Ebook')
+					->orderby('buku.id','desc')
+					->paginate(9);
+				}
+			}else{
+				$list = DB::table('buku')
+				->select(DB::raw('buku.*,kategori_buku.nama as namakategori'))
+				->leftjoin('kategori_buku','kategori_buku.id','=','buku.id_kategori')
+				->where([['buku.tipe','Book'],['buku.umum','ya']])
+				->orderby('buku.id','desc')
+				->paginate(9);
+	
+				$list_ebook = DB::table('buku')
+				->select(DB::raw('buku.*,kategori_buku.nama as namakategori'))
+				->leftjoin('kategori_buku','kategori_buku.id','=','buku.id_kategori')
+				->where([['buku.tipe','Ebook'],['buku.umum','ya']])
+				->orderby('buku.id','desc')
+				->paginate(9);
+			}
+		}
+		
+		
 		$count1 =DB::table('buku')->where('tipe','=','Book')->count();
 		$count2 =DB::table('buku')->where('tipe','=','Ebook')->count();
 		$count3 =DB::table('anggota')->count();
